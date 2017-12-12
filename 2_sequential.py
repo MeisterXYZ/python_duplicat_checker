8caefd5ae2b5be07992fa5adb15c176cb863408d
MeisterXYZ MB <raphael.drechsler@gmail.com>
2017-12-12 11:44:58 +0100

    Fin Alg 2

--- 2_sequential.py
+++ 2_sequential.py
@@ -17,24 +17,44 @@ except IOError:
 else:
     #initialize table
     table = []
+    # for handling the Sequence File Format
+    isSequenceString = False
     for line in f:
         #getting rid of line-break char \n
-        line = line[:-1]
-        if line != 'secquenceString':
-            #check if element is already in list
-            elementInList = False
-            for element in table:
-                if element == line:
-                    elementInList = True
-            #if so raise duplicate counter
-            if elementInList:
-                dupCount +=1
-            #otherwise append element to list
+            line = line[:-1]
+            #handling the Sequence File Format
+            if line[:9] == 'secquence':
+                if line == 'secquenceString':
+                    isSequenceString = True
+                else:
+                    isSequenceString = False
             else:
-                table.append(line)
-        else:
-            # To-Do: handle the sequence-File-format corretly
-            pass
+                if not isSequenceString:
+                    raise ValueError('Not all of the file-content was in "secquenceString"-format.')
+                else:
+                    #bad way
+                    '''
+                    #check if element is already in list
+                    elementInList = False
+                    i=0
+                    while (not elementInList) and (i<len(table)):
+                        if table[i] == line:
+                            elementInList = True 
+                            #if so raise duplicate counter
+                            dupCount +=1
+                        i+=1
+                    #otherwise append element to list
+                    if not elementInList:
+                        table.append(line)
+                    '''
+                    #good way
+                    #check if element is already in list
+                    if line in table:
+                        #if so raise duplicate counter
+                        dupCount +=1
+                    else:
+                        #otherwise append element to list
+                        table.append(line)
     f.close()
 
     #stop time measuring
@@ -43,4 +63,9 @@ else:
     print "dups found: ", dupCount
     print "elapsed time: ", end-start
 
-    #for practical use: implement something for writing data to file
+    #print result to file
+    with open ('Output_2_sequential.txt' , 'w') as f:
+        for i in table:
+            if i != None:
+                f.write(i)
+                f.write('\n')
